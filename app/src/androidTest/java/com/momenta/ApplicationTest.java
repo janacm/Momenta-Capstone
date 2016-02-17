9577aaa396fb4d90eb26784cf6451e8152ff3c69
Jonathan Coll <joesimpleplan@hotmail.com>
2016-02-17 13:27:36 -0500

    Test test

--- app/src/androidTest/java/com/momenta/ApplicationTest.java
+++ app/src/androidTest/java/com/momenta/ApplicationTest.java
@@ -1,13 +1,13 @@
-/*package com.momenta;
+package com.momenta;
 
 import android.app.Application;
 import android.test.ApplicationTestCase;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
- *
+ **/
 public class ApplicationTest extends ApplicationTestCase<Application> {
     public ApplicationTest() {
         super(Application.class);
     }
-}*/
+}
