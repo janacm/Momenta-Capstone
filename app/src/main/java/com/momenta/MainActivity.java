1ccdf610ff71d9b87b057242c448210124cf8636
nnam-ibe <nnam.ibe@gmail.com>
2016-02-18 16:50:09 -0500

    Implemented screen take over feature from dashboard

--- app/src/main/java/com/momenta/MainActivity.java
+++ app/src/main/java/com/momenta/MainActivity.java
@@ -4,14 +4,18 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    ManagerFragmentPagerAdapter fragementManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,8 +24,9 @@ public class MainActivity extends AppCompatActivity {
 
         // Get the ViewPager and set it's PagerAdapter so that it can display items
         ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
-        viewPager.setAdapter(new ManagerFragmentPagerAdapter(getSupportFragmentManager(),
-                MainActivity.this));
+        fragementManager = new ManagerFragmentPagerAdapter(getSupportFragmentManager(),
+                MainActivity.this);
+        viewPager.setAdapter(fragementManager);
 
         // Give the TabLayout the ViewPager
         TabLayout tabLayout = (TabLayout) findViewById(R.id.sliding_tabs);
@@ -48,5 +53,20 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void numberPicker(View v) {
+        Log.d("MainActivity", "Button Pressed");
+        Fragment fragment = fragementManager.getItem(0);
+        if (fragment == null || fragment.isVisible() ) {
+            Log.d("MainActivty", "Chai.");
+        }
+        Log.d("MainActivty", "Fragment Retrieved.");
+        if (fragment != null && fragment.isVisible()) {
+            if (fragment instanceof DashboardFragment) {
+                ((DashboardFragment) fragment).sendBroadcast(v);
+            }
+        }
+        Log.d("MainActivty", "Request deispatched");
     }
 }
