562ea40284a21e6dd2d693f7776a24801ee86c55
Jonathan Coll <joesimpleplan@hotmail.com>
2016-02-18 17:10:09 -0500

    Unit test in instrumentation test

---
+++ app/src/androidTest/java/com/momenta/ExampleUnitTest.java
@@ -0,0 +1,19 @@
+package com.momenta;
+
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
