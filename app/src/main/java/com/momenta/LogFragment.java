521e914449983db5df6e18c30c2a208d55bd90e7
nnam-ibe <nnam.ibe@gmail.com>
2016-03-08 13:48:30 -0500

    Implemented dialog for adding time

--- app/src/main/java/com/momenta/LogFragment.java
+++ app/src/main/java/com/momenta/LogFragment.java
@@ -1,16 +1,13 @@
 package com.momenta;
 
-import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.EditText;
 
 /**
