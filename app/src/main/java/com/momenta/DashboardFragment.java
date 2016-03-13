52e6631bd06cc64f0f82f3a7daced0a4d1100915
Khalid <khalidawil3@gmail.com>
2016-03-13 15:58:00 -0400

    Basic groundwork for Dashboard screen. Removed numberpicker from Dashboard screen.

--- app/src/main/java/com/momenta/DashboardFragment.java
+++ app/src/main/java/com/momenta/DashboardFragment.java
@@ -1,21 +1,12 @@
 package com.momenta;
 
 
-import android.app.AlarmManager;
-import android.app.PendingIntent;
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.NumberPicker;
-import android.widget.TextView;
-
-import java.util.Calendar;
 
 /**
  * Created by Joe on 2016-02-01.
@@ -46,28 +37,23 @@ public class DashboardFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         activityView= inflater.inflate(R.layout.fragment_dashboard, container, false);
-        TextView textView = (TextView) activityView.findViewById(R.id.textView3);
-        textView.setText("Fragment #" + mPage);
-        numberPicker = (NumberPicker) activityView.findViewById(R.id.numberPicker);
-        numberPicker.setMinValue(1);
-        numberPicker.setMaxValue(15);
         return activityView;
     }
 
-    public void sendBroadcast(View v){
-        Log.d("Dashboard", "Setting Alarm.");
-
-        int time = numberPicker.getValue() * 1000;
-        Log.d("Dashboard", "Setting Alarm..." + time);
-        Calendar cal = Calendar.getInstance();
-        cal.setTimeInMillis( cal.getTimeInMillis() + time );
-
-        Intent intentAlarm = new Intent(getContext(), Reciever.class);
-        PendingIntent pendingIntent = PendingIntent.getBroadcast(getContext(), 0, intentAlarm, PendingIntent.FLAG_UPDATE_CURRENT);
-        AlarmManager alarmManager = (AlarmManager) getContext().getSystemService(Context.ALARM_SERVICE);
-        alarmManager.set(AlarmManager.RTC_WAKEUP, cal.getTimeInMillis(), pendingIntent);
-        Log.d("Dashboard", "Alarm Set.");
-
-
-    }
+//    public void sendBroadcast(View v){
+//        Log.d("Dashboard", "Setting Alarm.");
+//
+//        int time = numberPicker.getValue() * 1000;
+//        Log.d("Dashboard", "Setting Alarm..." + time);
+//        Calendar cal = Calendar.getInstance();
+//        cal.setTimeInMillis( cal.getTimeInMillis() + time );
+//
+//        Intent intentAlarm = new Intent(getContext(), Reciever.class);
+//        PendingIntent pendingIntent = PendingIntent.getBroadcast(getContext(), 0, intentAlarm, PendingIntent.FLAG_UPDATE_CURRENT);
+//        AlarmManager alarmManager = (AlarmManager) getContext().getSystemService(Context.ALARM_SERVICE);
+//        alarmManager.set(AlarmManager.RTC_WAKEUP, cal.getTimeInMillis(), pendingIntent);
+//        Log.d("Dashboard", "Alarm Set.");
+//
+//
+//    }
 }
--- app/src/main/java/com/momenta/MainActivity.java
+++ app/src/main/java/com/momenta/MainActivity.java
@@ -1,15 +1,9 @@
 package com.momenta;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
-import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.util.Log;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -55,18 +49,18 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void numberPicker(View v) {
-        Log.d("MainActivity", "Button Pressed");
-        Fragment fragment = fragementManager.getItem(0);
-        if (fragment == null || fragment.isVisible() ) {
-            Log.d("MainActivty", "Chai.");
-        }
-        Log.d("MainActivty", "Fragment Retrieved.");
-        if (fragment != null && fragment.isVisible()) {
-            if (fragment instanceof DashboardFragment) {
-                ((DashboardFragment) fragment).sendBroadcast(v);
-            }
-        }
-        Log.d("MainActivty", "Request deispatched");
-    }
+//    public void numberPicker(View v) {
+//        Log.d("MainActivity", "Button Pressed");
+//        Fragment fragment = fragementManager.getItem(0);
+//        if (fragment == null || fragment.isVisible() ) {
+//            Log.d("MainActivty", "Chai.");
+//        }
+//        Log.d("MainActivty", "Fragment Retrieved.");
+//        if (fragment != null && fragment.isVisible()) {
+//            if (fragment instanceof DashboardFragment) {
+//                ((DashboardFragment) fragment).sendBroadcast(v);
+//            }
+//        }
+//        Log.d("MainActivty", "Request deispatched");
+//    }
 }
