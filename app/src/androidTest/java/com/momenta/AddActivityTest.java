0a3bcf4df219987c66018397da52917773598a6b
Jonathan Coll <joesimpleplan@hotmail.com>
2016-03-24 12:36:04 -0400

    add activity test for db testing

--- app/src/androidTest/java/com/momenta/AddActivityTest.java
+++ app/src/androidTest/java/com/momenta/AddActivityTest.java
@@ -3,9 +3,12 @@ import android.support.test.espresso.assertion.ViewAssertions;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import static junit.framework.Assert.assertEquals;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.util.Calendar;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.*;
@@ -17,10 +20,23 @@ import static android.support.test.espresso.matcher.ViewMatchers.*;
 @RunWith(AndroidJUnit4.class)
 public class AddActivityTest {
     @Rule public final ActivityTestRule<MainActivity> main = new ActivityTestRule<>(MainActivity.class);
+    DBHelper db = DBHelper.getInstance(main.getActivity());
+    @Test
+    public void addActivityDBTesting(){
+        String taskName = "TaskName";
+        int duration = 12;
+        Calendar deadline = Calendar.getInstance();
+        long id = db.insertTask(new Task(taskName, duration, deadline));
+
+        Task taskAdded = db.getTask((int) id);
+        assertEquals(taskName,taskAdded.getName());
+        assertEquals(duration,taskAdded.getTime());
+        assertEquals(deadline,taskAdded.getDeadline());
+    }
 
     @Test
     public void addActivity(){
-      //  onView(withId(R.id.new_activity_edit_text)).perform(typeText("Test activity"));
-        //onView(withId(R.id.new_activity_add_button)).perform(click());
+        onView(withId(R.id.new_activity_edit_text)).perform(typeText("Test activity"));
+        onView(withId(R.id.new_activity_add_button)).perform(click());
     }
 }
