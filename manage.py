fa31513419a11a952238e7af3ece37ed97980627
Vedha <vraje059@uottawa.ca>
2016-04-01 16:41:56 -0400

    Reverting manage.py to normal

--- manage.py
+++ manage.py
@@ -24,7 +24,7 @@ else:
     port = 5000
     host = '127.0.0.1'
 
-app = Eve(settings='settings.py')
+app = Eve(settings=my_settings)
 
 
 #End
@@ -44,3 +44,32 @@ client = MongoClient()
 client = MongoClient('mongodb://Vedha:test@ds055485.mongolab.com:55485/momenta-sandbox')
 
 db = client['momenta-sandbox']
+
+from datetime import datetime
+result = db.Momenta.insert_one(
+    {
+        "address": {
+            "street": "Janac is the best",
+            "zipcode": "Only on drugs",
+            "building": "Sometimes when hes not",
+            "coord": [-73.9557413, 40.7720266]
+        },
+        "borough": "Upmyass",
+        "cuisine": "Inmynose",
+        "grades": [
+            {
+                "date": datetime.strptime("2014-10-01", "%Y-%m-%d"),
+                "grade": "A",
+                "score": 11
+            },
+            {
+                "date": datetime.strptime("2014-01-16", "%Y-%m-%d"),
+                "grade": "B",
+                "score": 17
+            }
+        ],
+        "name": "Vella",
+        "restaurant_id": "41704620"
+    }
+)
+print result.inserted_id
