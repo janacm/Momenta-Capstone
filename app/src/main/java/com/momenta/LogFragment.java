43097893029c612770a585059af6bdc94e2c7ee6
nnam-ibe <nnam.ibe@gmail.com>
2016-09-15 10:59:32 -0400

    Removed Goals label from & changed fragment name to Goals

--- app/src/main/java/com/momenta/LogFragment.java
+++ app/src/main/java/com/momenta/LogFragment.java
@@ -13,7 +13,6 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.RadioGroup;
 import android.widget.TextView;
@@ -24,8 +23,6 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-import java.util.Calendar;
-
 /**
  * Created by Joe on 2016-01-31.
  * For Momenta
@@ -35,12 +32,6 @@ public class LogFragment extends Fragment {
     public static final String ARG_PAGE = "ARG_PAGE";
     public static final String ASC = "ASC";
     public static final String DESC = "DESC";
-    private EditText newActivity;
-    private EditText activityHour;
-    private EditText activityMinute;
-    private EditText activityDeadline;
-    private final Calendar deadlineCalendar = Calendar.getInstance();
-    private boolean deadlineSet = false;
     private String sortString = Task.LAST_MODIFIED;
     private String orderString = ASC;
     private helperPreferences helperPreferences;
@@ -83,43 +74,8 @@ public class LogFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_log, container, false);
         mRecyclerView = (RecyclerView) view.findViewById(R.id.activity_recycler_view);
 
-//        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getActivity());
-//        mRecyclerView.setLayoutManager(mLayoutManager);
-
-//=======
-
         setLayoutManger();
         mRecyclerView.setAdapter(mFirebaseAdapter);
-
-//        ImageButton addButton = (ImageButton)view.findViewById(R.id.new_activity_add_button);
-//        addButton.setOnClickListener(this);
-
-//        newActivity = (EditText) view.findViewById(R.id.new_activity_edit_text);
-
-//        activityHour = (EditText) view.findViewById(R.id.new_activity_hour_edit_text);
-//        activityMinute = (EditText) view.findViewById(R.id.new_activity_minute_edit_text);
-
-        //Add watcher to move focus to minute text view
-//        activityHour.addTextChangedListener(new TextWatcher() {
-//            @Override
-//            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-//            }
-//
-//            @Override
-//            public void onTextChanged(CharSequence s, int start, int before, int count) {
-//                if (s.length() == 2) {
-//                    activityMinute.requestFocus();
-//                }
-//            }
-//
-//            @Override
-//            public void afterTextChanged(Editable s) {
-//            }
-//        });
-
-//        activityDeadline = (EditText) view.findViewById(R.id.new_activity_deadline_edit_text);
-//        activityDeadline.setOnClickListener(this);
-//>>>>>>> firetrends
 
         return view;
     }
@@ -137,52 +93,9 @@ public class LogFragment extends Fragment {
         });
     }
 
-    @Override
-    public void onResume() {
-        super.onResume();
-    }
-
-//    @Override
-//    public void onClick(View v) {
-//        switch (v.getId()) {
-//<<<<<<< HEAD
-//            case R.id.new_activity_add_button:
-//                addActivity();
-//                httpRequest();
-//                return;
-//            case R.id.new_activity_deadline_edit_text:
-//                inputDeadline();
-//                break;
-//            case R.id.sort_button:
-//                sortDialog();
-//                break;
-//            case R.id.sort_order_layout:
-//                orderOnClick();
-//                break;
-//        }
-//    }
-
-//    private void orderOnClick() {
-//        if ( helperPreferences.getPreferences(DBHelper.ORDER, DBHelper.DESC).equals(DBHelper.ASC) ) {
-//            helperPreferences.savePreferences(DBHelper.ORDER, DBHelper.DESC);
-//            image.setBackgroundResource(R.drawable.ic_down_arrow);
-//        } else {
-//            helperPreferences.savePreferences(DBHelper.ORDER, DBHelper.ASC);
-//            image.setBackgroundResource(R.drawable.ic_up_arrow);
-//=======
-//            case R.id.new_activity_add_button:
-//                addActivity();
-//                httpRequest();
-//                return;
-//            case R.id.new_activity_deadline_edit_text:
-//                inputDeadline();
-//                break;
-//>>>>>>> firetrends
-//        }
-//    }
-
     /**
-     * On click method for the sort button
+     * On click method for the sort by menu option.
+     * Inflates the sort by dialog.
      */
     private void sortDialog() {
         LayoutInflater inflater = LayoutInflater.from(getContext());
@@ -276,138 +189,6 @@ public class LogFragment extends Fragment {
         sortDialogBuilder.create().show();
     }
 
-//<<<<<<< HEAD
-//    /**
-//     * Handler method for the add activity button on log fragment.
-//     */
-//    private void addActivity() {
-//        //If the text box is empty do nothing.
-//        if (!newActivity.getText().toString().trim().isEmpty()) {
-//            long hourField = Long.valueOf( "0" + activityHour.getText().toString() );
-//            long minuteField = Long.valueOf( "0" + activityMinute.getText().toString());
-//            Long totalMinutes = TimeUnit.MINUTES.convert(hourField, TimeUnit.HOURS)
-//                    + minuteField;
-//
-//            //If no goal is chosen, default to 2 hours. TODO Make default a preference
-//            if (totalMinutes == 0l) {
-//                totalMinutes = TimeUnit.MINUTES.convert(2, TimeUnit.HOURS);
-//            }
-//
-//            //If no deadline is chosen, default to one week from now. TODO Make default a preference
-//            if (!deadlineSet) {
-//                deadlineCalendar.setTimeInMillis( deadlineCalendar.getTimeInMillis()
-//                        + TimeUnit.MILLISECONDS.convert(7, TimeUnit.DAYS));
-//            }
-//
-//            Task task = new Task(newActivity.getText().toString(), totalMinutes.intValue(),
-//                    deadlineCalendar, Calendar.getInstance().getTimeInMillis(), Calendar.getInstance());
-//            DBHelper.getInstance(getContext()).insertTask(task);
-//
-//            //Reset input fields
-//            newActivity.setText("");
-//            activityHour.setText("");
-//            activityMinute.setText("");
-//            activityDeadline.setText("");
-//
-//            mAdapter.retrieveTasks();
-//            toast("Activity added!");
-//            deadlineSet = false;
-//        } else {
-//            toast(getContext().getString(R.string.toast_no_name_activity_added));
-//        }
-//    }
-//=======
-//    /**
-//     * Handler method for the add activity button on log fragment.
-//     */
-//    private void addActivity() {
-//        //If the text box is empty do nothing.
-//        if (!newActivity.getText().toString().trim().isEmpty()) {
-//            long hourField = Long.valueOf( "0" + activityHour.getText().toString() );
-//            long minuteField = Long.valueOf( "0" + activityMinute.getText().toString());
-//            Long totalMinutes = TimeUnit.MINUTES.convert(hourField, TimeUnit.HOURS)
-//                    + minuteField;
-//
-//            //If no goal is chosen, default to 2 hours.
-//            if (totalMinutes == 0L) {
-//                totalMinutes = TimeUnit.MINUTES.convert(2, TimeUnit.HOURS);
-//            }
-//
-//            //If no deadline is chosen, default to one week from now.
-//            if (!deadlineSet) {
-//                deadlineCalendar.setTimeInMillis( deadlineCalendar.getTimeInMillis()
-//                        + TimeUnit.MILLISECONDS.convert(7, TimeUnit.DAYS));
-//            }
-//
-//            Task task = new Task(newActivity.getText().toString(), totalMinutes.intValue(),
-//                    deadlineCalendar, Calendar.getInstance().getTimeInMillis(), Calendar.getInstance());
-//            String id = mFirebaseDatabaseReference.child(directory).push().getKey();
-//            task.setId(id);
-//            mFirebaseDatabaseReference.child(directory + "/" + id).setValue(task);
-//
-//            //Reset input fields
-//            newActivity.setText("");
-//            activityHour.setText("");
-//            activityMinute.setText("");
-//            activityDeadline.setText("");
-//
-//            toast("Activity added!");
-//            deadlineSet = false;
-//        } else {
-//            toast(getContext().getString(R.string.toast_no_name_activity_added));
-//        }
-//    }
-//>>>>>>> firetrends
-
-    /**
-     * Helper method to input the Deadline/Due date if an activity.
-     */
-//    private void inputDeadline() {
-//        Calendar cal = Calendar.getInstance();
-//        DatePickerDialog dialog = new DatePickerDialog(getActivity(), new DatePickerDialog.OnDateSetListener() {
-//            @Override
-//            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
-//                deadlineCalendar.set(year, monthOfYear, dayOfMonth);
-//                activityDeadline.setText(Task.getDateFormat(deadlineCalendar));
-//                deadlineSet = true;
-//            }
-//        }, cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(Calendar.DAY_OF_MONTH));
-//        dialog.getDatePicker().setMinDate(cal.getTimeInMillis());
-//        dialog.show();
-//    }
-
-//    /**
-//     * Convience method for toasting messages to the user
-//     * Toast message is set tot LENGTH_LONG.
-//     *
-//     * @param toToast the string to be displayed to the user
-//     */
-//    private void toast(String toToast) {
-//        Toast.makeText(getContext(), toToast, Toast.LENGTH_LONG).show();
-//    }
-//
-//    private void httpRequest() {
-//        String ping_url = "http://momenta.herokuapp.com/people";
-//        new HttpTask(ping_url, "GET") {
-//            @Override
-//            protected void onPostExecute(JSONObject json) {
-//                super.onPostExecute(json);
-//                try {
-//                    if (json != null) {
-//                        JSONArray ping_result = json.getJSONArray("_items");
-//                        JSONObject status_obj = ping_result.getJSONObject(0);
-//                        //TODO: Removed toast, T'was causing the build to fail on older devices
-////                        String status = status_obj.getString("_created");
-////                        toast(status);
-//                    }
-//                } catch (JSONException e) {
-//                    e.printStackTrace();
-//                }
-//            }
-//        }.execute();
-//
-//    }
-
     /**
      * Helper method to build a recycler adapter
      * @param sortBy the field to sort the taskas by
@@ -448,6 +229,9 @@ public class LogFragment extends Fragment {
         };
     }
 
+    /**
+     * Used to order the layout of the recycler view.
+     */
     private void setLayoutManger() {
         LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
         orderString = helperPreferences.getPreferences(DBHelper.ORDER, ASC);
@@ -462,6 +246,9 @@ public class LogFragment extends Fragment {
 
     }
 
+    /**
+     * ViewHolder class for each item in the recycler view.
+     */
     public static class TaskViewHolder extends RecyclerView.ViewHolder {
         public TextView name;
         public TextView timeSpent;
