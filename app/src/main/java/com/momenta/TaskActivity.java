7f1275261edd26187d636e71522cfdbbfd9548ef
nnam-ibe <nnam.ibe@gmail.com>
2016-10-24 22:29:02 -0400

    Change the fetchWeekData to only get data for past 7 days

--- app/src/main/java/com/momenta/TaskActivity.java
+++ app/src/main/java/com/momenta/TaskActivity.java
@@ -2,7 +2,6 @@ package com.momenta;
 
 import android.app.DatePickerDialog;
 import android.content.DialogInterface;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AlertDialog;
