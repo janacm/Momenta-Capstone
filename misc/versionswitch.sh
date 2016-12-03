aa3b7d4d9ee94760c00a93c5d5fe13746f541689
Joe <joesimpleplan@hotmail.com>
2016-12-02 20:44:04 -0500

    Script to increment version code automatically

---
+++ misc/versionswitch.sh
@@ -0,0 +1,7 @@
+MANIFEST_FILE='.\app\src\mainAndroidManifest.xml'
+VERSIONCODE=`grep versionCode $MANIFEST_FILE | sed 's/.*versionCode="//;s/".*//'`
+VERSIONNAME=`grep versionName $MANIFEST_FILE | sed 's/.*versionName="//;s/\.[0-9]*".*//'`
+NEWCODE=$BUILD_NUMBER
+NEWNAME=$VERSIONNAME.$BUILD_NUMBER
+echo "Updating Android build information. New version code: $NEWCODE - New version name: $NEWNAME";
+sed -i '' 's/versionCode = *"'$VERSIONCODE'"/versionCode="'$NEWCODE'"/; s/versionName *= *"[^"]"/versionName="'$NEWNAME'"/' $MANIFEST_FILE
